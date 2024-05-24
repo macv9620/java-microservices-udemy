b8ec5d94149da0f55c6631c6fe351f91e01714bd
mateo_vasco <mateo.castano@sofka.com.co>
2024-05-24 17:12:09 -0500

    Initial commit for S9

--- 15_1_springboot-servicio[usuarios]/springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ 15_1_springboot-servicio[usuarios]/springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -1,6 +1,5 @@
 package com.formacionbdi.springboot.app.usuarios.models.dao;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -8,12 +7,10 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
 
-@RepositoryRestResource(path="usuarios")
+@RepositoryRestResource(path="usuarios-info")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 
 	@RestResource(path="buscar-username")
 	public Usuario findByUsername(@Param("username") String username);
-	
-	@Query("select u from Usuario u where u.username=?1")
-	public Usuario obtenerPorUsername(String username);
+
 }
