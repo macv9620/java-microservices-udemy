9bfe7c1801614ee5b74f51069914c0f705cd7117
mateo_vasco <mateo.castano@sofka.com.co>
2024-05-28 07:58:25 -0500

    S13 Finished

--- 25_F_sleuth/zipkin.cmd
+++ 25_F_sleuth/zipkin.cmd
@@ -1,6 +1,6 @@
-@echo off
-set RABBIT_ADDRESSES=localhost:5672
-set STORAGE_TYPE=mysql
-set MYSQL_USER=zipkin
-set MYSQL_PASS=zipkin
-java -jar ./zipkin-server-2.12.9-exec.jar
+@REM @echo off
+@REM set RABBIT_ADDRESSES=localhost:5672
+@REM set STORAGE_TYPE=mysql
+@REM set MYSQL_USER=zipkin
+@REM set MYSQL_PASS=zipkin
+@REM java -jar ./zipkin-server-2.12.9-exec.jar
