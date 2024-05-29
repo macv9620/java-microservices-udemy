8c200c647ba9161062454444fbbd19d9c0bf1a2d
mateo_vasco <mateo.castano@sofka.com.co>
2024-05-28 19:48:16 -0500

    Docker compose config initial

---
+++ 27_F_Docker/zipkin.cmd
@@ -0,0 +1,6 @@
+@REM @echo off
+@REM set RABBIT_ADDRESSES=localhost:5672
+@REM set STORAGE_TYPE=mysql
+@REM set MYSQL_USER=zipkin
+@REM set MYSQL_PASS=zipkin
+@REM java -jar ./zipkin-server-2.12.9-exec.jar
